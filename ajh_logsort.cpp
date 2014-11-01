6057b1188bf35c69cbf63cfb4789e4df1fbfe8aa
Adam Howell <bigguywhokills@yahoo.com>
2014-10-31 18:18:40 -0600

    2

---
+++ ajh_logsort.cpp
@@ -0,0 +1,32 @@
+/*
+File Prologue
+Name: Adam Howell
+Course and section: CS2420-601
+Instructor: Reza Sanati Mehrizy
+Assignment: Program 07 - Graphs
+Date: 2014-04-14
+Description: This project will implement a graph and process data in that graph.
+
+I declare that the following source code was written by me, or provided by the instructor for this project. I understand that copying source
+code from another source constitutes cheating and that I will be given a zero on this assignment if I am found in violation of this policy.
+*/
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+
+const int PRECISION = 2;
+const int IGNORE = 4096;
+const string MENU1 = "Welcome to Adam's logfile sort utility.";
+const string MENU2 = "\n";
+
+
+int main()
+{
+	return 0;
+}
